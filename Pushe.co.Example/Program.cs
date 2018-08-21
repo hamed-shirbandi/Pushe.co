ce2248a448208a8fb682ccabf832113357b7b514
hamed shirbandi <hamed.shirbandi@gmail.com>
2018-08-21 16:12:29 +0430

    Add project files.

---
+++ Pushe.co.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pushe.co.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
